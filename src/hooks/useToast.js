48ecc7e331b91bde0f8003f7c386d9a850ac746d
Tushar22101 <tushar.singh@lumiq.ai>
2024-08-15 16:37:12 +0530

    added auto remove feature to the toaster

--- src/hooks/useToast.js
+++ src/hooks/useToast.js
@@ -5,16 +5,25 @@ export function useToast() {
     const dispatch = useToastDispatchContext();
 
     // Function to add a toast
-    function toast(type, message) {
+    function toast(type, message, duration = 5000) {
         const id = Date.now(); // Unique ID for the toast
         dispatch({
             type: "ADD_TOAST",
             toast: {
                 type, // Type of the toast (e.g., success, error)
                 message, // The message to display
-                id // Unique identifier for the toast
+                id, // Unique identifier for the toast
             }
         });
+
+        // Set a timeout to automatically delete the toast after the duration
+        setTimeout(() => {
+            dispatch({
+                type: "DELETE_TOAST",
+                id
+            });
+        }, duration);
+
     }
 
     return toast; // Return the toast function to be used in components
