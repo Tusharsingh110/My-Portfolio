93a0a65080334c6f826462e1870b26400b2c2a6c
Tushar22101 <tushar.singh@lumiq.ai>
2024-08-17 01:34:22 +0530

    WIP button added for new skill addition

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import skillsdata from "./SkillsData"
+import { useSelector } from 'react-redux';
+import { Button } from 'antd';
+import { PlusOutlined,CloseCircleOutlined } from "@ant-design/icons";
+
 
 
 export default function Skills() {
+  const {isLoggedIn, isAdmin} = useSelector(state => state.auth);
 
   const allSkills = skillsdata.map(skill => (
     // key=skill.id,
-    <SkillCard key={skill.id} source={skill.imgsrc}  label={skill.label}/>
+    <SkillCard key={skill.id} id={skill.id} source={skill.imgsrc}  label={skill.label}/>
   ) )
 
   return (
@@ -16,7 +21,9 @@ export default function Skills() {
         <div className='md:text-xl sm:text-[25px] text-white text-center grid lg:gap-5 md:gap-3 lg:grid-cols-5 sm:grid-cols-3'>
 
           {allSkills}
-
+          {isLoggedIn && isAdmin === 'T' && <SkillCard>
+            <Button className='h-full w-full rounded-xl' icon={<PlusOutlined />} type='dashed'>Add Skill</Button>
+            </SkillCard>}
         </div>
       </div>
     </div>
@@ -25,8 +32,20 @@ export default function Skills() {
 
 
 
-function SkillCard({source, label}) {
+function SkillCard({id, source, label, children}) {
+  const {isLoggedIn, isAdmin} = useSelector(state => state.auth);
+  const [mouseEnter, setMouseEnter] = useState(false);
+  const handleMouseEnter = () => {
+    setMouseEnter(true);
+  }
+  const handleMouseLeave = () => {
+    setMouseEnter(false);
+  }
   return (
-    <div className=' h-[150px] bg-[#EDF3FD] dark:bg-[#33373f] p-4 rounded-2xl shadow-xl drop-shadow-lg w-[150px] mx-auto my-2 hover:scale-105 transition duration-300'><img src={source}  alt={label} /></div>
+    <>
+    <div className=' h-[150px] bg-[#EDF3FD] dark:bg-[#33373f] p-4 rounded-2xl shadow-xl drop-shadow-lg w-[150px] mx-auto my-2 hover:scale-105 transition duration-300' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><img src={source}  alt={label} />{children}
+    {isLoggedIn && isAdmin=== 'T' && mouseEnter && id && <Button className='absolute -top-2 -right-2 w-2 text-xl rounded-full' ><CloseCircleOutlined/></Button>}
+    </div>
+    </>
   )
 }
